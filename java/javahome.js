034238d92d36dbde39c9080dfa29c5cac09d3854
kavi678 <kavipatel111.kp@gmail.com>
2019-04-17 18:41:57 +0930

    many changes

--- java/javahome.js
+++ java/javahome.js
@@ -5,3 +5,23 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
+window.onscroll = function() {myFunction()};
+
+var lnav = document.getElementById("lnav");
+var sticky = lnav.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    lnav.classList.add("sticky")
+  } else {
+    lnav.classList.remove("sticky");
+  }
+}
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
